573a9da1b1a02de978ab91d81d913e5890e5af91
semigura <shotaro-ishigaki@gaji.jp>
2020-06-25 17:06:26 +0900

    Add Button to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Buttons from './containers/Buttons'
 import Calculator from './containers/Calculator'
 
 function App() {
   return (
     <div className="App">
+      <Buttons />
       <div className="container">
         <Calculator />
       </div>
