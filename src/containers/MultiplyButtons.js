b4a03c65f0715b02feb2ce12bc3611c3eabe82a5
semigura <shotaro-ishigaki@gaji.jp>
2020-06-26 13:35:35 +0900

    Use grid and container

--- src/containers/MultiplyButtons.js
+++ src/containers/MultiplyButtons.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 function MultiplyButtons() {
   const [buttons, setButtons] = useState(["Button"]);
@@ -7,15 +9,23 @@ function MultiplyButtons() {
     setButtons([...buttons, "Button"]);
   };
   return (
-    <div>
-      {buttons.map((button, index) => (
-        <>
-          <Button key={index} variant="contained" onClick={handleButtonClick}>
-            Default
-          </Button>
-        </>
-      ))}
-    </div>
+    <Container>
+      <Grid container spacing={1} justify="space-between">
+        {buttons.map((button, index) => (
+          <>
+            <Grid container item xs={2}>
+              <Button
+                key={index}
+                variant="contained"
+                onClick={handleButtonClick}
+              >
+                Default
+              </Button>
+            </Grid>
+          </>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
