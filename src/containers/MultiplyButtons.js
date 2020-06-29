6cd50571aea1a26b1d132982dbc7ed9a19df1ed2
semigura <shotaro-ishigaki@gaji.jp>
2020-06-29 16:17:23 +0900

    Fix to generate different buttons

--- src/containers/MultiplyButtons.js
+++ src/containers/MultiplyButtons.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
 function MultiplyButtons() {
   const [buttons, setButtons] = useState(["Button"]);
   const handleButtonClick = () => {
-    setButtons([...buttons, "Button"]);
+    let number = Math.floor(Math.random() * 2);
+    let components = ["Button", "Button2"];
+    setButtons([...buttons, components[number]]);
   };
   return (
     <Container>
@@ -14,13 +16,25 @@ function MultiplyButtons() {
         {buttons.map((button, index) => (
           <>
             <Grid container item xs={2}>
-              <Button
-                key={index}
-                variant="contained"
-                onClick={handleButtonClick}
-              >
-                Default
-              </Button>
+              {button === "Button" && (
+                <Button
+                  key={index}
+                  variant="contained"
+                  onClick={handleButtonClick}
+                >
+                  Default
+                </Button>
+              )}
+              {button === "Button2" && (
+                <Button
+                  key={index}
+                  variant="contained"
+                  color="primary"
+                  onClick={handleButtonClick}
+                >
+                  Default
+                </Button>
+              )}
             </Grid>
           </>
         ))}
